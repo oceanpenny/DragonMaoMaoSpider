182c86c0f148cfda4052ad7781b8658cc27c709b
Hello@Mr.Ocean <penghq_mail@163.com>
2018-08-10 11:22:33 +0800

    ProxyMiddleware and DoubanSpider

--- DragonMaoMaoSpider/DragonMaoMaoSpider/middlewares.py
+++ DragonMaoMaoSpider/DragonMaoMaoSpider/middlewares.py
@@ -4,21 +4,51 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-
+from datetime import datetime, timedelta
 import random
-from scrapy import signals
+from twisted.web._newclient import ResponseNeverReceived
+from twisted.internet.error import TimeoutError, ConnectionRefusedError, ConnectError
+
 from DragonMaoMaoSpider.anticrawl.user_agents import agents
 from DragonMaoMaoSpider.anticrawl.proxy import proxy, http_key, https_key
 
 class UserAgentMiddleware(object):
-    """ 换User-Agent """
     def process_request(self, request, spider):
         agent = random.choice(agents)
         request.headers["User-Agent"] = agent
 
 class ProxyMiddleware(object):
+    DONT_RETRY_ERRORS = (TimeoutError, ConnectionRefusedError, ResponseNeverReceived, ConnectError, ValueError)
+    def __init__(self):
+        self.last_proxy_time = datetime.now()
+        self.recover_interval = 20
 
     def process_request(self, request, spider):
-        request.meta['dont_redirect'] = True
-        request.meta['proxy'] = proxy.get_proxy(http_key)
-        print('proxy: ',request.meta['proxy'])
+        if datetime.now() > (self.last_proxy_time + timedelta(minutes=self.recover_interval)):
+            request.meta['change_proxy'] = True
+            self.last_no_proxy_time = datetime.now()
+
+        #request.meta['dont_redirect'] = True
+        if "change_proxy" in request.meta.keys() and request.meta["change_proxy"]:
+            print(request.url)
+            if request.url.startswith('https://'):
+                request.meta['proxy'] = proxy.get_proxy(https_key)
+            elif request.url.startswith('http://'):
+                request.meta['proxy'] = proxy.get_proxy(http_key)
+            request.meta['change_proxy'] = False
+            print('proxy: ',request.meta['proxy'])
+
+    def process_response(self, request, response, spider):
+        if response.status != 200 \
+                and (not hasattr(spider, "website_possible_httpstatus_list") \
+                     or response.status not in spider.website_possible_httpstatus_list):
+            print("response status not in spider.website_possible_httpstatus_list")
+            new_request = request.copy()
+            new_request.meta['change_proxy'] = True
+            new_request.dont_filter = True
+            return new_request
+        else:
+            return response
+
+    def process_exception(self, request, exception, spider):
+        print('request exception: ', request.url)
--- DragonMaoMaoSpider/DragonMaoMaoSpider/spiders/doubanspider.py
+++ DragonMaoMaoSpider/DragonMaoMaoSpider/spiders/doubanspider.py
@@ -1,9 +1,6 @@
-import urllib.request
 
 from scrapy_redis.spiders import RedisSpider
-from bs4 import BeautifulSoup
 from DragonMaoMaoSpider.items import BookItem
-from scrapy.selector import Selector
 from DragonMaoMaoSpider.urlgen.generators import DouBanUrlGenerator
 from scrapy.http import Request
 
@@ -16,14 +13,11 @@ class Spider(RedisSpider):
     def parse(self, response):
         bookItem = BookItem()
         booklist = response.xpath('//div[@class="mod book-list"]')
-        #print(booklist)
         for book_info in booklist.xpath('.//dd'):
             bookItem['title'] = book_info.xpath('./a[@class="title"]/text()').extract()[0]
             desc = book_info.xpath('.//div[@class="desc"]/text()').extract()[0]
-            print(desc)
             desc_list = desc.split('/')
             book_url = book_info.xpath('./a[@class="title"]/@href').extract()[0]
-            #book_url = book_info.find('a', {'class': 'title'}).get('href')
             try:
                 bookItem['author_info'] = '作者/译者： ' + '/'.join(desc_list[0:-3])
             except:
@@ -33,20 +27,15 @@ class Spider(RedisSpider):
             except:
                 bookItem['pub_info'] = '出版信息： 暂无'
             try:
-                bookItem['rating'] = book_info.find('span', {'class':'rating_nums'}).string.strip()
+                bookItem['rating'] = book_info.xpath('.//span[@class="rating_nums"]').extract()[0]
             except:
                 bookItem['rating'] ='0.0'
-            #print(bookItem['title'])
-            #print(bookItem['author_info'])
-            #print(bookItem['pub_info'])
-            #print(bookItem['rating'])
-            #print(bookItem.author_info)
-            print("boourl: " + book_url)
             yield (Request(url=book_url, meta={"item": bookItem}, callback=self.parsbook_url, dont_filter=True))
-        #yield bookItem
 
     def parsbook_url(self, response):
         item = response.meta['item']
-        people_num = response.xpath('//div[@class="rating_sum"]//span').extract()[1]
-        print('num: ' + people_num)
-        #people_num = soup.find('div', {'class': 'rating_sum'}).findAll('span')[1].string.strip()
+        try:
+            item['people_num'] = response.xpath('//div[@class="rating_sum"]//span')[1].xpath('./text()').extract()[0]
+        except:
+            item['people_num'] = '0'
+        yield(item)
